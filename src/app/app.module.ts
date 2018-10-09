db55c82d380b611013a697ed6364eb5ce6f0614f
Xenos141 <arturomr2001@hotmail.com>
2018-10-09 08:05:40 -0500

    papps again

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { CalculoPage } from '../pages/calculo/calculo';
 import { MiPage } from '../pages/mi/mi';
 import { RoboticaPage } from '../pages/robotica/robotica';
 import { ComPage } from '../pages/com/com';
+import { PappsPage } from '../pages/papps/papps';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import { ComPage } from '../pages/com/com';
     CalculoPage,
     MiPage,
     RoboticaPage,
-    ComPage
+    ComPage,
+    PappsPage
   ],
   imports: [
     BrowserModule,
@@ -41,6 +43,7 @@ import { ComPage } from '../pages/com/com';
     MiPage,
     RoboticaPage,
     ComPage,
+    PappsPage
   ],
   providers: [
     StatusBar,
